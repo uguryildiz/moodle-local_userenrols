dfbb92bbdc4f9d20719d79b42814e37cddb5be11
uguryildiz <yildiz@visustek.com.tr>
2014-03-17 09:44:14 +0200

    test

--- lib.php
+++ lib.php
@@ -60,7 +60,7 @@
     	
     	$coursenode = $PAGE->navigation->find($PAGE->course->id, navigation_node::TYPE_COURSE);
     	
-    	$coursenode->add(
+    	$PAGE->settingsnav->add(
     			get_string('IMPORT_MENU_LONG', local_userenrols_plugin::PLUGIN_NAME),
     			local_userenrols_plugin::get_plugin_url('import', $PAGE->course->id),
     			navigation_node::TYPE_SETTING,
